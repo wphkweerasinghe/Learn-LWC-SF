0e46730515e387578a4465052c38a36c5fc5e96d
unknown <hkalhara777@gmail.com>
2023-04-29 10:48:21 +0530

    Add dynamic images to each and every cards

--- force-app/main/default/lwc/itemList/itemList.js
+++ force-app/main/default/lwc/itemList/itemList.js
@@ -2,12 +2,12 @@ import { LightningElement, wire } from 'lwc';
 // Access getItem method
 import getItems from '@salesforce/apex/ItemController.getItems';
 // Import static picture from salesforce static resource
-import sample_pic from '@salesforce/resourceUrl/sampleItemPicture';
+//import sample_pic from '@salesforce/resourceUrl/sampleItemPicture';
 
 export default class ItemList extends LightningElement {
     // Local variables
     items;
-    item_pic = sample_pic;
+    // item_pic = sample_pic;
     @wire(getItems)
     wiredItems({ error, data }) {
         if (data) {
